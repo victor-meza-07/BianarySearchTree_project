93871acd0783e01e058f83808058cc9055c038a0
victor-meza-07 <victord.meza@gmail.com>
2020-02-28 15:06:51 -0600

    Init

---
+++ BianarySearchTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BianarySearchTree
+{
+    class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node()
+        {
+
+        }
+
+        public void DisplayNode() 
+        {
+            Console.Write(Data + " ");
+        }
+    }
+}
